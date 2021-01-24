0f69ef74385172b3494c427bdf8711df33b3186b
Karan Shetty <shettykaran2110@gmail.com>
2021-01-25 01:33:54 +0530

    Added some functions

--- vector.cpp
+++ vector.cpp
@@ -10,7 +10,8 @@ void Vector::vec() {
    * v.size(), v.capacity(), v.reserve(), v.shrink_to_fit(),
    * v.resize(), v.max_size(), v.clear(), v.empty()
    *
-   * v.at(), v.front(), v.back(), v.push_back(), v.pop_back()
+   * v.at(), v.front(), v.back(), v.push_back(), v.pop_back(), v.emplace(),
+   * v.emplace_back()
    *
    * v.assign(), v.insert()
    *
@@ -62,4 +63,58 @@ void Vector::vec() {
   v7.max_size();  // returns max size of vector
   v7.clear();     // Clears the vector
   v7.empty();     // Check if vector is empty
+
+  // ------------------------ Single element access ---------------------//
+  // [], s.at(), s.front(), s.back(), s.push_front(), s.push_back()
+  v7 = {1, 2, 3, 4, 5, 6};
+  v7[2];       // returns 3
+  v7[2] = 10;  // v7: {1, 2, 10, 4, 5, 6}
+  v7[10];      // Undefined behaviour if index is out of range
+
+  v7.at(2);       // returns 10
+  v7.at(2) = 12;  // v7: {1, 2, 12, 4, 5, 6}
+  // v7.at(10);       // Throws exception out_of_range
+
+  v7.front();        // returns 1 | If empty, undefined behaviour
+  v7.back();         // returns 6 | If empty, undefined behaviour
+  v7.push_back(17);  // v7: {1, 2, 12, 4, 5, 6, 17}
+  v7.pop_back();     // returns & removes last element | v7: {1, 2, 12, 4, 5, 6}
+                     // If empty, undefined behaviour
+
+  // Advantage of emplace is, it does in-place insertion and avoids an
+  // unnecessary copy of object. For primitive data types, it does not matter
+  // which one we use. But for objects, use of emplace() is preferred for
+  // efficiency reasons.
+  v7.emplace_back(14);        // v7: {1, 2, 12, 4, 5, 6, 17, 14}
+  v7.emplace(v7.begin(), 4);  // v7: {4, 1, 2, 12, 4, 5, 6, 17, 14}
+                              // (pos, value)
+
+  // -------------------------- Ranged access ----------------------- //
+  // v.assign(), v.insert()
+
+  // v.assign() => (size, value)
+  v7 = {};
+  v7.assign(7, 100);  // v7: {100, 100, 100, 100, 100, 100, 100}
+                      // (size, value)
+  v7.assign(a, a + sizeof(a) / sizeof(int));  // from arrays
+  v7.assign(v6.begin(), v6.end());            // from vectors
+  v7.assign({1, 2, 3});  // from initializer list | v7: {1, 2, 3}
+
+  // v.insert() => (pos, value) | (pos, size, value)
+  v7 = {10, 20, 30, 40};
+  auto it = v7.insert(v7.begin(), 3);  // v7: {3, 10, 20, 30, 40}
+                                       // (pos, value)
+  v7.insert(it, 2);                    // v7: {2, 3, 10, 20, 30, 40}
+  v7.insert(v7.begin() + 2, {6, 7});   // v7: {2, 3, 6, 7, 10, 20, 30, 40}
+                                       // initializer list
+  it = v7.insert(v7.begin(), 1, 17);   // insert 17 one time at front
+                                       // v7: {17, 2, 3, 6, 7, 10, 20, 30, 40}
+                                       // (pos, size, value)
+  v7.insert(it, 2, 4);  // v7: {4, 4, 17, 2, 3, 6, 7, 10, 20, 30, 40}
+
+  // ----------------------- Others -------------------- //
+  v7 = {1, 2, 3, 4, 5, 6};
+  v7.data();  // Returns a direct pointer to the memory array used internally by
+              // the vector to store its owned elements.
+  v7.swap(v6);
 }
